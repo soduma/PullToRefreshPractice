63422b711a600381c3f4ea0d7003227b9e221098
soduma <soduma@naver.com>
2021-08-10 15:09:10 +0900

    pull to refresh

--- PullToRefreshPractice/ViewController.swift
+++ PullToRefreshPractice/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        refreshSomthing()
     }
-
-
+    
+    func refreshSomthing() {
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.tintColor = .systemRed
+        tableView.refreshControl?.attributedTitle = NSAttributedString(string: "반갑습니당")
+        tableView.refreshControl?.addTarget(self, action: #selector(update), for: .valueChanged)
+    }
+    
+    @objc func update() {
+        tableView.refreshControl?.endRefreshing()
+        tableView.reloadData()
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = String(indexPath.row)
+        let number = Date().timeIntervalSince1970
+        cell.textLabel?.text = String(number)
+        return cell
+    }
+}
